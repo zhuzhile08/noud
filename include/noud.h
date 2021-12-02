0b87a9ac84be35fed8b43c1ff5343f5663027b77
Zhile <zhuzhile08@gmail.com>
2021-12-02 21:15:28 +0100

    add some functions

--- include/noud.h
+++ include/noud.h
@@ -1,18 +1,23 @@
 #pragma once
 
 #include <string>
+#include <map>
 
 namespace noud {
     struct Node {
         std::string name = "node";
         Node* parent = NULL;
+        std::map <std::string, Node*> children;
 
         Node();
         Node(Node* parent);
 
         void destroy();
 
+        void add_child(Node* newChild);
+
         void add_to_beginning(Node root);
-        void add_behind(Node* newParent);
+        void add_to(Node* newParent);
+        void add_between(Node* back);
     };
 }
--- src/noud.cpp
+++ src/noud.cpp
@@ -4,7 +4,16 @@ namespace noud {
     Node::Node(Node* parent) : parent(parent) { }
 
     void Node::destroy() {
+        for (auto [key, child] : children) {
+            child->parent = parent;
+            children.erase(key);
+        }
         delete parent;
+    }
+
+    void Node::add_child(Node* newChild) {
+        newChild->parent = this;
+        children.insert(std::pair<std::string, Node*>(newChild->name, newChild));
     }
 
     void Node::add_to_beginning(Node root) {
@@ -12,7 +21,12 @@ namespace noud {
         root.parent = this;
     }
 
-    void Node::add_behind(Node* newParent) {
+    void Node::add_to(Node* newParent) {
         parent = newParent;
+    }
+
+    void Node::add_between(Node* back) {
+        parent = back->parent;
+        add_child(back);
     }
 }

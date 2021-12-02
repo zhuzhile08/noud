e1ad72c4ba6ba18735dcda306f8f598c56d59461
Zhile <zhuzhile08@gmail.com>
2021-12-02 20:47:59 +0100

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programming-Related/C++ Projects/Libraries/Noud
-# Build directory: D:/Programming-Related/C++ Projects/Libraries/Noud/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
